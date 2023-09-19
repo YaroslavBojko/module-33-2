f0269925f37367063bbd568a70457f5c840c9bd7
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-09-19 20:54:45 +0300

    fish

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <stdlib.h>
 
-class 
+class Fish
+{
+
+};
+
+class Boot
+{
+
+};
+
+class Field
+{
+public:
+    bool fish;
+    bool boot;
+};
 
 int main() {
-    int field
+    std::srand(time(nullptr));
+    Field field[9] = {0};
+
+    field[rand() % 9].fish = new Fish();
+
+    for (int i = 0; i < 9; ++i)
+    {
+        std::cout << field[i].fish << " " ;
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < 9; ++i)
+    {
+        std::cout << field[i].boot << " " ;
+    }
+    std::cout << std::endl;
+
+
+
 }
